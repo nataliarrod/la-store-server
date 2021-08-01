1b20c4de4ad6ece19617528acfdd3873b8225810
Natalia Rodriguez <nataliarrod@gmail.com>
2021-08-01 01:00:19 -0500

    products route updated, enable cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const productRouter = require("./src/routes/product");
 
 const database = require("./src/database");
@@ -8,8 +9,9 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 database();
+app.use(cors());
 app.use(express.json());
 
-app.use("/products", productRouter);
+app.use("/store", productRouter);
 
 app.listen(port, () => console.log("App listening localhost:8000"));
